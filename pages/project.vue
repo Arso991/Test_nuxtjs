f0ed064446db54a9cec74c9eb5cb258f15f4b387
Arso991 <arsogn991@gmail.com>
2023-10-31 21:37:33 +0100

    page details projet

--- pages/project.vue
+++ pages/project.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="flex gap-10">
         <div class="w-2/3 py-4 ps-4 pe-8 bg-white rounded-lg border border-teal-100">
             <h1 class="text-2xl font-semibold mb-8">Detail projet</h1>
             <div class="flex gap-2">
@@ -39,15 +39,46 @@
             </div>
         </div>
         <div>
-            <div>
+            <div class="bg-white p-4 flex flex-col justify-center border border-teal-100 rounded-lg">
+                <div class="w-full flex justify-center">
+                    <div class="overflow-hidden object-cover w-16 h-16 rounded-full mb-5">
+                        <img class="w-[100%] h-[100%]" src="/5.jpg" alt="">
+                    </div>
+                </div>
 
+                <small class="flex items-center justify-center gap-2 mb-4 font-semibold"><span>Marcel Houedo</span>
+                    <div class="h-2 w-2 rounded-full bg-slate-300"></div><span class="text-teal-100">1 projet en
+                        cours</span>
+                </small>
+                <p class="font-semibold mb-4 text-center text-sm">00229 97 23 34 90</p>
+                <div class="border-t border-slate-300 text-center pt-4">
+                    <button class="p-2 bg-teal-100 text-xs rounded-lg text-teal-400">Envoyez message</button>
+                </div>
             </div>
-            <div>
-
+            <div class="bg-white p-5 border border-teal-100 rounded-lg">
+                <h1 class="text-2xl text-center font-medium mb-8">Paiement</h1>
+                <div class="flex items-center ps-8">
+                    <div class="w-5 h-5 rounded-full bg-teal-100"></div>
+                    <div class="w-14 h-1 bg-slate-100"></div>
+                    <div class="w-4 h-4 rounded-full bg-slate-100"></div>
+                    <div class="w-12 h-1 bg-slate-100"></div>
+                    <div class="w-4 h-4 rounded-full bg-slate-100"></div>
+                    <div class="w-12 h-1 bg-slate-100"></div>
+                    <div class="w-4 h-4 rounded-full bg-slate-100"></div>
+                    <div class="w-12 h-1 bg-slate-100"></div>
+                    <div class="w-4 h-4 rounded-full bg-slate-100"></div>
+                </div>
+                <p class="text-left text-xs mt-2 mb-8">Frais dépôt transfert</p>
             </div>
-            <div>
-                
+            <div class="bg-white p-5 border border-teal-100 rounded-lg">
+                <h1 class="text-2xl text-center font-medium mb-8">Avancement</h1>
+                <div>
+                    <!-- <Pie :data="chartData" :config="config" /> -->
+                </div>
             </div>
         </div>
     </div>
 </template>
+<script setup>
+
+</script>
