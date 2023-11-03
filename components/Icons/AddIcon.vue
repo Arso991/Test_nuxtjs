f202a35b7ad3c2c7b913b298fe0d52ac2671a7bb
Arso991 <arsogn991@gmail.com>
2023-11-03 13:29:04 +0100

    dossier icons supprimé

--- components/Icons/AddIcon.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="flex items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="15" height="15">
-            <path
-                d="M23,11H13V1a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V11H1a1,1,0,0,0-1,1H0a1,1,0,0,0,1,1H11V23a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13H23a1,1,0,0,0,1-1h0A1,1,0,0,0,23,11Z" />
-        </svg>
-    </div>
-</template>
